8f8acbafbb58914aca13f59cfd1a57818bdbe94a
Jiri Travnicek <jiri.travnicek@outlook.com>
2019-08-23 15:20:28 +0200

    configure redis

--- config/bundles.php
+++ config/bundles.php
@@ -3,4 +3,5 @@
 return [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
     Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
+    \Snc\RedisBundle\SncRedisBundle::class => ['all' => true],
 ];
